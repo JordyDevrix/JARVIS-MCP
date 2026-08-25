439ab9e6d161861e0d9a9f6f4de00e3fe7090d6e
Jordy <contact@jordydevrix.com>
2026-08-26 00:04:11 +0200

    init

---
+++ src/main/kotlin/com/clovercloud/jarvis/controllers/DevelopmentController.kt
@@ -0,0 +1,20 @@
+package com.clovercloud.jarvis.controllers
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/development")
+@Tag(name = "Development")
+class DevelopmentController {
+
+    @ApiResponse(responseCode = "200", description = "Returns a simple health check response")
+    @GetMapping("/health")
+    fun health(): ResponseEntity<String> = ResponseEntity("Healthy", HttpStatus.OK)
+
+}
