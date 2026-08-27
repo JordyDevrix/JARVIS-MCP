3fe9b528c4a9bcc26f16bf4c993a9974cde6e328
Jordy Devrix <132071885+JordyDevrix@users.noreply.github.com>
2026-08-27 17:43:08 +0200

    refactor: import LocationResponse from responses folder and remove inline data class

--- src/main/kotlin/com/clovercloud/jarvis/controllers/LocationController.kt
+++ src/main/kotlin/com/clovercloud/jarvis/controllers/LocationController.kt
@@ -8,16 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-
-data class LocationResponse(
-    val city: String,
-    val country: String,
-    val countryCode: String,
-    val latitude: Double,
-    val longitude: Double,
-    val timezone: String,
-    val description: String
-)
+import com.clovercloud.jarvis.responses.LocationResponse
 
 @RestController
 @RequestMapping("/v1/tools/location")
